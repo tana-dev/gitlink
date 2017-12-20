bc4073be40bf8b7c8ac457ba2df0a359aa8cc0d6
S.T <tanashun@tanakashunichi-no-MacBook-Air.local>
2017-12-20 23:35:31 +0900

    repair build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,22 @@
+# set
+OS=${1}
+
 # resource file copy
 cp -R ../src/resources ../bin/gitlocal/
 cp ../src/config/user.json ../bin/gitlocal/config/
 
+# remove binary file
+rm ../bin/gitlocal/gitlocal*
+
 # compile
 cd ../src/main/
-GOOS=linux GOARCH=amd64 go build -o ../../bin/gitlocal/gitlocal main.go
-GOOS=windows GOARCH=amd64 go build -o ../../bin/gitlocal/gitlocal.exe main.go
+case ${OS} in
+    "linux" ) GOOS=linux GOARCH=amd64 go build -o ../../bin/gitlocal/gitlocal main.go
+    ;;
+    "windows" ) GOOS=windows GOARCH=amd64 go build -o ../../bin/gitlocal/gitlocal.exe main.go
+    ;;
+    "mac" ) GOOS=darwin GOARCH=amd64 go build -o ../../bin/gitlocal/gitlocal main.go
+    ;;
+    * ) echo "No compile"
+    ;;
+esac
